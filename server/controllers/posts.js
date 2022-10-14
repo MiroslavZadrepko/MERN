2d14b4739e89ef318bd531bf391d943605fb69eb
Miroslav Zadrepko <miroslav.zadrepko@gmail.com>
2022-10-14 11:50:19 +0000

    2nd

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,13 +1,14 @@
-import PostMessage from "../models/postMessage";
+import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
     try {
 
         const postMessages = await PostMessage.find();
-        res.status(200).json(postMessages)
+        
+        res.status(200).send(postMessages)
 
     } catch (error) {
-        res.status(404).json({ message: error.message })
+        res.status(408).json({ message: error.message })
     }
 }
 
