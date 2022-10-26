c86dd09527a67c3e60090456fc42176946e014ac
MiroslavZadrepko <miroslav.zadrepko@gmail.com>
2022-10-26 15:15:48 +0200

    before pull

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ import postRoutes from './routes/posts.js';
 const app = express();
  
 app.use(cors());
-app.use(express.json())
-app.use('/posts', postRoutes)
+app.use(express.json());
+app.use('/posts', postRoutes);
 
 const DB_URI = 'mongodb+srv://root:root@cluster0.pp3mm.mongodb.net/test';
 const PORT = process.env.PORT || 5000;
