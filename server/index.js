70fbb9e972b3e29f0d9cade103b0d5da5ef5c031
MiroslavZadrepko <miroslav.zadrepko@gmail.com>
2022-10-26 15:31:24 +0200

    6th 	modified:   server/controllers/posts.js 	modified:   server/index.js

--- server/index.js
+++ server/index.js
@@ -6,13 +6,8 @@ import postRoutes from './routes/posts.js';
 const app = express();
  
 app.use(cors());
-<<<<<<< HEAD
 app.use(express.json());
 app.use('/posts', postRoutes);
-=======
-app.use(express.json())
-app.use('/', postRoutes)
->>>>>>> 6b7f8378ab55ef80722387faf45dc8fc128d20a4
 
 const DB_URI = 'mongodb+srv://root:root@cluster0.pp3mm.mongodb.net/test';
 const PORT = process.env.PORT || 5000;
