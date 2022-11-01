b766d3f7145d7389e669999e4c1a13ae86515462
Miroslav Zadrepko <miroslav.zadrepko@gmail.com>
2022-11-01 13:02:55 +0000

    starting redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,33 +2,37 @@ import React from 'react';
 import Form from './components/Form/Form'
 import Posts from './components/Posts/Posts'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import useStyles from './appStyle';
 
 function App() {
+
+  const classes = useStyles();
+
   return (
     <Container maxwidth="lg" >
-     
-        <AppBar position='static' color='inherit'>
-          <Typography variant='h2' align='center'>Memories</Typography>
-          <p align='center'>
-            <img src='https://raw.githubusercontent.com/adrianhajdin/project_mern_memories/master/client/src/images/memories.png?token=AF56X74XONEUGZ4FD2FUIA27UURPI' width={60} alt="icon" />
-          </p>
-        </AppBar>
 
-        <Grow in>
-          <Container>
-            <Grid container justifyContent='space-between' alignItems='stretch' spacing={2} >
+      <AppBar className={classes.appBar} position='static' color='inherit'>
+        <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
+        <p align='center'>
+          <img className={classes.image} src='https://raw.githubusercontent.com/adrianhajdin/project_mern_memories/master/client/src/images/memories.png?token=AF56X74XONEUGZ4FD2FUIA27UURPI' width={60} alt="icon" />
+        </p>
+      </AppBar>
 
-              <Grid item xs={6} xm={6}>
-                <Posts />
-              </Grid>
+      <Grow in>
+        <Container>
+          <Grid container justifyContent='space-between' alignItems='stretch' spacing={2} >
 
-              <Grid item xs={6} xm={4}>
-                <Form />
-              </Grid>
-
+            <Grid item xs={6} xm={6}>
+              <Posts />
             </Grid>
-          </Container>
-        </Grow>
+
+            <Grid item xs={6} xm={4}>
+              <Form />
+            </Grid>
+
+          </Grid>
+        </Container>
+      </Grow>
 
     </Container>
   );
